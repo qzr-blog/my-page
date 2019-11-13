804a02e3d25ea755244e397f1368cfbe91033cd1
Qzr <z5021996@vip.qq.com>
2019-11-13 18:03:43 +0800

    :construction: 修改api格式

--- src/api/base.ts
+++ src/api/base.ts
@@ -1,9 +1,7 @@
 const apis: any = {
   basic: {
-    getOverview: '/overview', //获取文章总览
-    getInfo: '/info', //获取文章信息
-    createInfo: 'createInfo',//新建文章
-    saveInfo: '/saveInfo', //获取文章信息
+    overview: '/overview', //获取文章总览
+    info: '/info', //获取文章信息
   }
 }
 
--- src/api/content/basic.ts
+++ src/api/content/basic.ts
@@ -1,19 +1,22 @@
-import base from '../base'
-import axios from '../../common/js/axios'
+import base from "../base";
+import axios from "../../common/js/axios";
+
+const info = base.basic.info,
+  overview = base.basic.overview;
 
 const basic: object = {
   getInfo() {
-    return axios.get(`${base.basic.getInfo}`)
+    return axios.get(`${info}`);
   },
   getOverview() {
-    return axios.get(`${base.basic.getOverview}`)
+    return axios.get(`${overview}`);
   },
   saveInfo() {
-    return axios.post(`${base.basic.saveInfo}`)
+    return axios.put(`${info}`);
   },
   createInfo(data: Object) {
-    return axios.post(`${base.basic.createInfo}`, data)
-  },
-}
+    return axios.post(`${info}`, data);
+  }
+};
 
-export default basic
+export default basic;
